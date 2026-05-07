d2a44dc983b4e7635bb47f4a456bb42db115faa1
Carlos Henrique <crodriguescavalcante@gmail.com>
2026-05-07 12:14:09 -0300

    chore: Spring Boot project initiated

---
+++ src/test/java/dev/ch/librivoxis/LibrivoxisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ch.librivoxis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrivoxisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

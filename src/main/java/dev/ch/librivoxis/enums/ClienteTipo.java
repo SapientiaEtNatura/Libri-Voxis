ee579f2f2108bf58187c8da5ddb44a48a9f741b3
Carlos Henrique <crodriguescavalcante@gmail.com>
2026-05-07 16:33:25 -0300

    feat: add ClienteTipo and EmprestimoStatus enums

---
+++ src/main/java/dev/ch/librivoxis/enums/ClienteTipo.java
@@ -0,0 +1,6 @@
+package dev.ch.librivoxis.enums;
+
+public enum ClienteTipo {
+    PADRAO,
+    PROFESSOR
+}
---
+++ src/main/java/dev/ch/librivoxis/enums/EmprestimoStatus.java
@@ -0,0 +1,9 @@
+package dev.ch.librivoxis.enums;
+
+public enum EmprestimoStatus {
+    AGENDADO,
+    EM_ANDAMENTO,
+    DEVOLVIDO,
+    ATRASADO,
+    CANCELADO
+}
